dc577450099e92a9617cb8b9576cbbeb7ab85a68
Akos Kitta <kittaakos@typefox.io>
2019-01-29 13:10:23 +0100

    GH-3: Removed `src-gen` and `xtend-gen` from the version control.Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- com.ge.research.sadl.darpa.aske.dialog.parent/com.ge.research.sadl.darpa.aske.dialog.ui/xtend-gen/com/ge/research/sadl/darpa/aske/ui/syntaxcoloring/DialogSemanticHighlightingCalculator.java
+++
@@ -1,7 +0,0 @@
-package com.ge.research.sadl.darpa.aske.ui.syntaxcoloring;
-
-import com.ge.research.sadl.ui.syntaxcoloring.SadlSemanticHighlightingCalculator;
-
-@SuppressWarnings("all")
-public class DialogSemanticHighlightingCalculator extends SadlSemanticHighlightingCalculator {
-}
