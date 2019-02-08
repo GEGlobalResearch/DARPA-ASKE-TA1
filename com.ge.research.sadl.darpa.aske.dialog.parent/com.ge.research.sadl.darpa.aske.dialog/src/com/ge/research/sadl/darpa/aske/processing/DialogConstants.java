d1c1647c58f6c849887f61233dd3fa145363d0b2
Andy Crapo <crapo@ge.com>
2019-02-08 16:26:09 -0500

    created constants class

---
+++ com.ge.research.sadl.darpa.aske.dialog.parent/com.ge.research.sadl.darpa.aske.dialog/src/com/ge/research/sadl/darpa/aske/processing/DialogConstants.java
@@ -0,0 +1,6 @@
+package com.ge.research.sadl.darpa.aske.processing;
+
+public class DialogConstants {
+	public static final String LAST_DIALOG_COMMAND = "LastDialogCommand";
+	public static final String DIALOG_ANSWER_PROVIDER = "DialogAnswerProvider";
+}
