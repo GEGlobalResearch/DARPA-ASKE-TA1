88c25202edf4825ade919a462211b694668909b2
awcrapo <crapo@research.ge.com>
2019-10-24 10:14:15 -0400

    mods to be compatible with OwlToSadl

--- com.ge.research.sadl.darpa.aske.dialog.parent/com.ge.research.sadl.darpa.aske.dialog/src/com/ge/research/sadl/darpa/aske/curation/AnswerCurationManager.java
+++ com.ge.research.sadl.darpa.aske.dialog.parent/com.ge.research.sadl.darpa.aske.dialog/src/com/ge/research/sadl/darpa/aske/curation/AnswerCurationManager.java
@@ -1949,7 +1949,7 @@ public class AnswerCurationManager {
 		if (trgt instanceof NamedNode && whn == null) {
 			String answer;
 			try {
-				answer = whatIsNamedNode(resource, theModel, getOwlModelsFolder(), (NamedNode)trgt);
+				answer = whatIsNamedNode(resource, theModel, modelName, getOwlModelsFolder(), (NamedNode)trgt);
 			}
 			catch (Exception e) {
 				answer = e.getMessage();
@@ -2138,7 +2138,7 @@ public class AnswerCurationManager {
 		return "Unable to find method to display graph";
 	}
 
-	private String whatIsNamedNode(org.eclipse.emf.ecore.resource.Resource resource, OntModel theModel, String modelFolder, NamedNode lastcmd) throws ConfigurationException, ExecutionException, TranslationException, InvalidNameException, ReasonerNotFoundException, QueryParseException, QueryCancelledException {
+	private String whatIsNamedNode(org.eclipse.emf.ecore.resource.Resource resource, OntModel theModel, String modelName, String modelFolder,  NamedNode lastcmd) throws ConfigurationException, ExecutionException, TranslationException, InvalidNameException, ReasonerNotFoundException, QueryParseException, QueryCancelledException, OwlImportException {
 		// what is NamedNode?
 		NamedNode nn = (NamedNode) lastcmd;
 		NodeType typ = nn.getNodeType();
@@ -2151,7 +2151,7 @@ public class AnswerCurationManager {
 //			if (answer.length() == len) {
 //				answer.append(" is a class");
 //			}
-			answer.append(getOwlToSadl(theModel).classToSadl(nn.getURI()));
+			answer.append(getOwlToSadl(theModel, modelName).classToSadl(nn.getURI()));
 //			isFirstProperty = addDomainAndRange(resource, nn, isFirstProperty, answer);
 //			addQualifiedCardinalityRestriction(resource, nn, isFirstProperty, answer);		
 			Object ctx = ((NamedNode)lastcmd).getContext();
@@ -2189,6 +2189,11 @@ public class AnswerCurationManager {
 			logger.debug(answer.toString());
 			return answer.toString();
 		}
+	}
+
+	private OwlToSadl getOwlToSadl(OntModel theModel, String modelName) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	private OwlToSadl getOwlToSadl(OntModel model) {
